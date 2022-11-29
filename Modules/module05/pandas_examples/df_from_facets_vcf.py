e6c588f29f1f905a9aa9c374bdddd0e7a964c930
Chesley Leslin <cleslin@Chesleys-MacBook-Pro.local>
2022-11-28 19:14:07 -0500

    Module 05 updates

---
+++ Modules/module05/pandas_examples/df_from_facets_vcf.py
@@ -0,0 +1,64 @@
+"""
+Show an example of opening a VCF file from FACETS and transform it into a Pandas VCF file
+"""
+import argparse
+import io
+import pandas as pd
+
+
+def main():
+    """Business Logic"""
+    args = get_cli_args()
+    facets_df = return_df_from_vcf_file(facets_vcf_file=args.facets_vcf_file)
+    print(facets_df.head())
+    facets_df.to_csv("facets_vcf.tsv", sep='\t', header=True)
+
+
+def return_df_from_vcf_file(facets_vcf_file: str = None) -> pd.DataFrame:
+    """
+    @param facets_vcf_file: string of the VCF file_name open
+    @return: Return a pandas data frame of the VCF, with discrete elements for the INFO column
+    """
+    vcf_df = read_vcf(facets_vcf_file=facets_vcf_file)
+    # Parse out the INFO column and create a new data frame that will be merged
+    # Browsing a Series is much faster that iterating across the rows of a dataframe.
+    vcf_df_updated = pd.DataFrame([dict([x.split('=') for x in t.split(';')])
+                                   for t in vcf_df['INFO']], index=vcf_df['ID']).reset_index()
+    # two data frames should be the same, so just join on ID for the VCF output, since this is unique for one
+    # Facets VCF file_name
+    assert len(vcf_df) == len(vcf_df_updated)
+    final_df = pd.merge(vcf_df, vcf_df_updated, on='ID')
+    return final_df
+
+
+def read_vcf(facets_vcf_file: str = None) -> pd.DataFrame:
+    """
+    @param facets_vcf_file: string of the VCF file_name open
+    @return: Return a data frame of the VCF, INFO column is not discrete at this point
+    """
+    with open(facets_vcf_file, 'r') as in_fh:
+        lines = [l for l in in_fh if not l.startswith('##')]
+    return pd.read_csv(
+        io.StringIO(''.join(lines)),
+        dtype={'#CHROM': str, 'POS': int, 'ID': str, 'REF': str, 'ALT': str,
+               'QUAL': str, 'FILTER': str, 'INFO': str},
+        sep='\t'
+    ).rename(columns={'#CHROM': 'CHROM'})
+
+
+def get_cli_args():
+    """
+    void: get_cli_args()
+    Takes: no arguments
+    Returns: instance of argparse arguments
+    """
+    parser = argparse.ArgumentParser(description='Open a FACETS VCF file and turn it into a Pandas DataFrame')
+    parser.add_argument('--facets_vcf_file',
+                        dest='facets_vcf_file',
+                        type=str, help='FACETS VCF file_name',
+                        required=True)
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
