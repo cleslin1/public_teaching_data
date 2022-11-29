e480cf9b2fb3be049f63140adcce22937d64554e
Chesley Leslin <cleslin@Chesleys-MacBook-Pro.local>
2022-11-28 19:00:37 -0500

    Module 05

--- Modules/module05/pandas_examples/df_from_json.py
+++ Modules/module05/pandas_examples/df_from_json.py
@@ -1,6 +1,13 @@
 import pandas as pd
 df = pd.read_json('human_EGFR_variants.json')
-print(df)
+print(f"Size of the df, {len(df)}")
+print(df.head())
 
-
+# print out all unique values
 print(df['source'].unique())
+
+# print out all unique values
+print(df['consequence_type'].unique())
+
+# print out examples from the df where consequence_type == stop_lost
+print(df[df['consequence_type'] == 'stop_lost'])
